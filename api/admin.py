babbcf79ddb75b1e442456a260b3d00ab7cc6e92
Александър Неделчев <aleks.nedelchev@gmail.com>
2018-02-25 14:31:00 +0200

    register models on admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import *
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Problem)
+admin.site.register(Idea)
+admin.site.register(Event)
+admin.site.register(UserEvent)
+admin.site.register(Comment)
+admin.site.register(ReportHistory)
+admin.site.register(UserSubscription)
+admin.site.register(Continent)
+admin.site.register(Country)
+admin.site.register(City)
